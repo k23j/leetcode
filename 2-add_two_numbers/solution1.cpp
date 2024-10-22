b233abc15ddaf17670b1573a99d293e45511e177
k23j <passos.dev.k@gmail.com>
2024-10-22 13:08:39 -0300

    Update solution1.cpp

--- 2-add_two_numbers/solution1.cpp
+++ 2-add_two_numbers/solution1.cpp
@@ -12,7 +12,7 @@ struct ListNode {
 };
 
 const vector<pair<vector<int>,vector<int>>> testList = {
-  {{9,9,9,9,9,9,9},{9,9,9,9}},
+  {{9,9,9,9},{9,9,9,9,9,9,9}},
   {{2,4,3},{5,6,4}},
 };
 
@@ -33,18 +33,33 @@ ListNode* createList(vector<int> valueVec) {
 
 
 class Solution {
-public:
+  private:
+    size_t getListSize(const ListNode* l, size_t currentSize = 0) {
+      if (l->next == nullptr) return currentSize + 1;
+      return getListSize(l->next, currentSize+1);
+    }
+
+    void orderLists(ListNode*& l1, ListNode*& l2) {
+      size_t list1Size = getListSize(l1);
+      size_t list2Size = getListSize(l2);
+
+      if (list1Size >= list2Size) return;
+
+      ListNode* temp = l1;
+      l1 = l2;
+      l2 = temp;
+    }
+  public:
   ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+    orderLists(l1,l2);
     ListNode* head = l1;
 
     int carry = 0;
 
-    while (l1 != nullptr || l2 != nullptr) {
+    while (true) {
       int currentVal = carry;
 
-      if (l1 != nullptr) {
-        currentVal += l1->val;
-      }
+      currentVal += l1->val;
 
       if (l2 != nullptr) {
         currentVal += l2->val;
@@ -54,8 +69,14 @@ public:
 
       carry = currentVal / 10;
 
-      if (l1 != nullptr) {
-        l1 = l1->next;
+      if (l1->next == nullptr) {
+        break;
+      }
+
+      l1 = l1->next;
+
+      if (l2 == nullptr & carry == 0) {
+        break;
       }
 
       if (l2 != nullptr) {
